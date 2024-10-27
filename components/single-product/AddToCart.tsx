b7b1408d45f81e2e5668a87cf3003d032eec99d3
kimalex415 <kimalex415@gmail.com>
2024-10-26 22:52:38 -0700

    breadcrumbs and product page completed

---
+++ components/single-product/AddToCart.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+function AddToCart() {
+	return (
+		<Button className="capitalize mt-8" size="lg">
+			add to cart
+		</Button>
+	);
+}
+
+export default AddToCart;
